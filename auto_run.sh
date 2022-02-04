9a3712dfe0a3266618fa73c2e0b84851aac501a6
technotic <micfogas@users.noreply.github.com>
2022-02-04 00:35:22 -0800

    updated to match guider 2 update

--- auto_run.sh
+++ auto_run.sh
@@ -3,8 +3,9 @@
 EXE=guider2-arm
 UNVS=universal-arm
 PID=000C
-QTAPP="/opt/flashforge/exe/guider2"
+QTAPP="/opt/flashforge/exe/guiderII"
 OLD_QTAPP=${QTAPP}"-arm"
+NEW_QTAPP=${QTAPP}".bin "
 #ifconfig eth0 10.33.23.146
 udhcpc -i eth0 &
 #RUNPARA=" 1 debug -qws "
@@ -13,73 +14,12 @@ RUNPARA=" 1 -qws "
 for i in 1 2 3 4 5;
 do
   if [ $i == 5 ]; then
-	mount -t vfat -o,codepage=936,iocharset=utf8 /dev/sda /mnt
-	  if [ $? -ne 0 ]; then
-			echo "mount /dev/sda to /mnt failed"
-			continue
-	  else
-		ls -al /etc > /mnt/out.etc.log
-		if [ -f /mnt/$PID ] || [ -f /mnt/$UNVS ]; then
-			if [ -f /mnt/flashforge_init.sh ]; then
-				 echo "found /mnt/flashforge_init.sh"
-				 chmod a+x /mnt/flashforge_init.sh
-				 /mnt/flashforge_init.sh
-				 umount /mnt
-				 break
-			fi
-			umount /mnt
-		fi
-		umount /mnt
-	  fi
-  else
-	  if [ ! -e /dev/sda$i ]; then
-		 echo "sda$i not exist"
-		 continue
-	  fi
-	  mount -t vfat -o,codepage=936,iocharset=utf8 /dev/sda$i /mnt
+	mount -t vfat -o,codepage=936,iocharset=utf8 /dev/sda$i /mnt
 	  if [ $? -ne 0 ]; then
 			echo "mount /dev/sda$i to /mnt failed"
 			continue
 	  else
-	  ls -1t /mnt/creator3*.zip
-		if [ $? -eq 0 ];then
-			UPDATEFILE=`ls -1t /mnt/guider2*.zip | head -n 1`
-			if [ -f $UPDATEFILE ];then
-				echo "find update file: ${UPDATEFILE}"
-				rm -rf /data/update
-				cp -a ${UPDATEFILE} /data/
-				if [ $? -ne 0 ];then
-					rm -rf /data/guider2*.zip
-					sync
-					umount /mnt
-					break
-				fi
-				sync
-				mkdir -p /data/update
-				sync
-				SRCFILE="/data/`basename ${UPDATEFILE}`"
-				if [ -f ${SRCFILE} ];then
-					unzip -o ${SRCFILE} -d /data/update/
-					sync
-					rm -rf ${SRCFILE}
-					if [ -f /data/update/$PID ] || [ -f /data/update/$UNVS ]; then
-						echo "found /mnt/flashforge_init.sh"
-						chmod a+x /data/update/flashforge_init.sh
-						/data/update/flashforge_init.sh
-						if [ $? -eq 0 ];then
-							echo "update failed"
-							umount /mnt
-							rm -rf /data/update
-							sleep 10
-						fi
-						umount /mnt
-						rm -rf /data/update
-						break
-					fi
-				fi
-			fi
-		fi
-		if [ -f /mnt/$PID ] || [ -f /mnt/$UNVS ]; then
+		ls -al /etc > /mnt/out.etc.log
 			if [ -f /mnt/flashforge_init.sh ]; then
 				 echo "found /mnt/flashforge_init.sh"
 				 chmod a+x /mnt/flashforge_init.sh
@@ -88,21 +28,18 @@ do
 				 break
 			fi
 			umount /mnt
-		fi
-		umount /mnt
 	  fi
-  fi
 done
 
 echo "Gpio WiFi Power On"
 echo 258 >/sys/class/gpio/export
-echo out >/sys/class/gpio/gpio258/direction
-echo 1 > /sys/class/gpio/gpio258/value
-echo 0 > /sys/class/gpio/gpio258/value
+echo out >/sys/class/gpio/gpio195/direction
+echo 1 > /sys/class/gpio/gpio195/value
+echo 0 > /sys/class/gpio/gpio195/value
 
 echo 259 >/sys/class/gpio/export
-echo out >/sys/class/gpio/gpio259/direction
-echo 1 > /sys/class/gpio/gpio259/value
+echo out >/sys/class/gpio/gpio194/direction
+echo 1 > /sys/class/gpio/gpio194/value
 
 ifconfig wlan0 up
 wpa_supplicant -d -Dwext -i wlan0 -c /etc/wpa_supplicant.conf -B
@@ -161,7 +98,7 @@ echo "export qt"
 export QTDIR=$WORK_DIR/qt4.8.6-none
 export LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
 export QT_QPA_PLATFORM_PLUGIN_PATH=$QTDIR/plugins
-export QT_QPA_PLATFORM=linuxfb:tty=/dev/fb0:size=480x864:mmsize=25x15:offset=0
+export QT_QPA_PLATFORM=linuxfb:tty=/dev/fb0:size=480x320:mmsize=25x15:offset=0
 export QWS_DISPLAY=transformed:rot270:LinuxFB:mmWidth144:mmHeight72:0
 export QT_QPA_FONTDIR=$QTDIR/lib/fonts
 #export LD_PRELOAD=$WORK_DIR/libiconv-1.14-none/lib/preloadable_libiconv.so:$TSLIBDIR/lib/libts.so
@@ -176,22 +113,7 @@ if [ ! -d /media ]; then
   mkdir /media
 fi
 
-if [ ! -d /data/pics ]; then
-  mkdir /data/pics
-else
-  rm -f  /data/pics/*
-fi
 
 
 chmod  777 /opt/flashforge/exe/mydaemon.out
 /opt/flashforge/exe/mydaemon.out &
-if [ -f ${OLD_QTAPP} ] ;then
-chmod  777  ${OLD_QTAPP}
-echo "test start "${OLD_QTAPP}
-${OLD_QTAPP}${RUNPARA}&
-sleep 1;
-rm -rf /opt/kernel
-rm -rf /opt/control
-rm -rf /opt/library
-rm -rf /opt/software
-fi

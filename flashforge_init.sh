7dd5f257b737d70ee935c0ba501ea2dce5eabba6
technotic <micfogas@users.noreply.github.com>
2022-02-04 00:48:12 -0800

    updated to match guider 2

--- flashforge_init.sh
+++ flashforge_init.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-EXE=guider2-arm
-KERNEL=uImage-guider2*
-LOG=guider2-log-pwn.log
-M3HEX=Guider2.hex
+EXE=guiderII-arm
+KERNEL=uImage-guider*
+LOG=guiderII-log-pwn.log
+M3HEX=GuiderII.hex
 MYDAEMON=mydaemon.out
-MACHINE=guider2
-PID=0004
+MACHINE=guiderII
 MACHINE_ARCH=armv5tejl
 
 # Get the working directory
@@ -43,14 +42,88 @@ then
 fi
 
 # Install Samba
-if [ -f $WORKDIR/scripts/run/samba.sh ]
-then
-  source $WORKDIR/scripts/run/samba.sh
-fi
+#if [ -f $WORKDIR/scripts/run/samba.sh ]
+#then
+#  source $WORKDIR/scripts/run/samba.sh
+#fi
 
 sync
 
 sleep 1
+
+echo "Flash Forge Cloud Switch OFF" >> $LOG
+sed -i '1c OFF' /opt/flashforge/cloud-ff
+sync
+
+echo "Ethernet MAC Set" >> $LOG
+find $WORKDIR/mac_addrs -type f -print0 | xargs -0 dos2unix
+Ethernet_MAC=`sed -n '/ifconfig eth0 hw ether/p' /etc/init.d/rcS | cut -d ' ' -f5 | grep -E '[0-9a-fA-F]{2}(:[0-9a-fA-F]{2}){5}'`
+echo "Ethernet_MAC="$Ethernet_MAC >> $LOG
+
+if [ ! $Ethernet_MAC ]; then
+	MAC_ADDR_H=`sed -n '1p' $WORKDIR/mac_addrs`
+	echo "None MAC MAC_ADDR_H="$MAC_ADDR_H >> $LOG
+	
+	if [ $MAC_ADDR_H ]; then
+		sed -i '/ifconfig/d' /etc/init.d/rcS
+		sed -i '3a ifconfig eth0 hw ether '"$MAC_ADDR_H" /etc/init.d/rcS
+		sync
+	else
+		echo "None MAC Address or \"mac_addrs\" file" >> $LOG
+#                exit -1
+#		halt -f
+	fi
+	
+	MAC_ADDR_R=`sed -n '2p' $WORKDIR/mac_addrs`
+	echo "None MAC MAC_ADDR_R="$MAC_ADDR_R >> $LOG
+	
+	if [ "$MAC_ADDR_R" \> "$MAC_ADDR_H" ]; then
+		machex=`awk 'BEGIN{FS=OFS=":";$0=ARGV[1];$NF=sprintf("%.2X",("0x"$NF)+1);print}' $MAC_ADDR_H`
+		echo "machex="$machex >> $LOG
+		sed -i '1c '"$machex" $WORKDIR/mac_addrs
+		sync
+	else
+		rm -f $WORKDIR/mac_addrs
+		sync
+	fi
+	
+	find /etc/init.d/rcS -type f -print0 | xargs -0 dos2unix
+	echo "Ethernet MAC Set Success" >> $LOG
+else
+	MAC_ADDR_H=`sed -n '1p' $WORKDIR/mac_addrs`
+	echo "Has MAC MAC_ADDR_H="$MAC_ADDR_H >> $LOG
+	
+	MAC_ADDR_R=`sed -n '2p' $WORKDIR/mac_addrs`
+	echo "Has MAC MAC_ADDR_R="$MAC_ADDR_R >> $LOG
+	
+	if [ "$Ethernet_MAC" \> "$MAC_ADDR_H" ]; then
+		machex=`awk 'BEGIN{FS=OFS=":";$0=ARGV[1];$NF=sprintf("%.2X",("0x"$NF)+1);print}' $Ethernet_MAC`
+		echo "machex="$machex >> $LOG
+		sed -i '1c '"$machex" $WORKDIR/mac_addrs
+		sync
+	fi
+	if [ "$Ethernet_MAC" \> "$MAC_ADDR_R" ]; then
+		rm -f $WORKDIR/mac_addrs
+		sync
+	fi
+fi
+sync
+sleep 1
+echo "RSA cryptographic keys" >> $LOG
+rm -rf /opt/openssl-1.0.2d-none/bin/key*
+sed -i '1c OFF' /opt/flashforge/cloud-username-pin
+sed -i '4d' /opt/flashforge/cloud-username-pin
+sync
+
+if [ ! -f /opt/openssl-1.0.2d-none/bin/key.priv ]; then
+	echo 'abcde' | ssh-keygen -t rsa -b 2048 -f /opt/openssl-1.0.2d-none/bin/key.priv
+fi
+
+if [ ! -f /opt/openssl-1.0.2d-none/bin/key.pub ]; then
+	ssh-keygen -e -m PEM -f /opt/openssl-1.0.2d-none/bin/key.priv > /opt/openssl-1.0.2d-none/bin/key.pub
+fi
+
+sync
 
 # Tell user it's done on LCD
 echo "Display pwnd on lcd" >> $LOG
